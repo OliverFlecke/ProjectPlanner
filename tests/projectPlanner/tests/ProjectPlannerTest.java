98acd2816006a57b13b684c1e9572a7b561eb550
Oliver Fleckenstein <Oliverfl@live.dk>
2015-03-16 20:15:56 +0100

    Added test folder

---
+++ tests/projectPlanner/tests/ProjectPlannerTest.java
@@ -0,0 +1,19 @@
+package projectPlanner.tests;
+
+import org.junit.*;
+/**
+ * @author Oliver Fleckenstein
+ *
+ */
+public class ProjectPlannerTest {
+
+	@Before
+	public void setup() {
+		
+	}
+	
+	@After
+	public void tearDown() {
+		
+	}
+}
