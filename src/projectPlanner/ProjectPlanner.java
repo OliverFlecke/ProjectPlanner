8792e7920c49d7efa6df8e4f1a728c4879936e6b
sebakeaaen <sebakeaaen@gmail.com>
2015-04-13 17:28:56 +0200

    projectplanner

--- src/projectPlanner/ProjectPlanner.java
+++ src/projectPlanner/ProjectPlanner.java
@@ -8,13 +8,30 @@ import projectPlanner.users.*;
 public class ProjectPlanner {
 
 	private User currentUser;
+	private boolean isLoggedIn;
 	
 	public boolean login(String username, String password) throws UserLoginException, Exception {
 		currentUser = User.getUserByUsername(username);
 		if (currentUser == null) {
-			throw new UserLoginException("Username not found");
+			throw new UserLoginException("Invalid username or password");
 		}
-		
-		return false;
+		if(!currentUser.checkPassword(password)){
+			throw new UserLoginException("Invalid username or password");
+		}
+		setIsLoggedIn(true);
+		return isLoggedIn; 
+	}
+	
+	public void logout(){
+		this.currentUser=null;
+		setIsLoggedIn(false);
+	}
+	public void setIsLoggedIn(boolean setter){
+		isLoggedIn=setter;
+	}
+	public boolean getIsLoggedIn() {
+		return isLoggedIn;
 	}
 }
+
+
