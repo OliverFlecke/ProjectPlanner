b56bbb50b7534f1ec953ad689ded8b746d538a36
sebakeaaen <sebakeaaen@gmail.com>
2015-05-04 16:23:46 +0200

    waitWindow

--- src/projectPlanner/view/projectPanel/waitWindow.java
+++ src/projectPlanner/view/projectPanel/waitWindow.java
@@ -1,26 +1,31 @@
 package projectPlanner.view.projectPanel;
 
-import java.io.IOException;
+import javax.swing.BoxLayout;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
-public class waitWindow {
-	//create barChart
-			new Thread() {
-	            @Override
-	            public void run() {
-	            	
-	            	try {
-						
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}                
-	                latch.countDown();
-	            }
-	        }.start();
-	        try {
-	            latch.await();
-	        } catch (InterruptedException ex) {
-	           ex.printStackTrace();
-	        }
-
+public class WaitWindow extends JDialog{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1603108265194880949L;
+	
+	public WaitWindow(){
+		super();
+		JPanel panel = new JPanel();
+		this.add(panel);
+		JLabel label = new JLabel("Please wait while the report is generated");
+		panel.add(label);
+		JProgressBar bar = new JProgressBar();
+		panel.add(bar);
+		bar.setIndeterminate(true);
+		setTitle("Generating report");
+		setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+		pack();
+		
+		
+	}
 }
