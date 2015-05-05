4b1b4970729a3fafa13364c1da4f6d51ca35c1e6
Oliver Fleckenstein <Oliverfl@live.dk>
2015-05-05 20:05:53 +0200

    Fixed naming

--- tests/projectPlanner/database/ActivityDatabaseManagerTests.java
+++ tests/projectPlanner/database/ActivityDatabaseManagerTests.java
@@ -63,7 +63,7 @@ public class ActivityDatabaseManagerTests {
 	public void saveActivityToDatabase() throws SQLException {
 		// This test can not be run each time. This will try to save the activity to the database
 		// We primary look for exceptions
-		Activity activity = new Activity("Test activity 2", project);
+//		Activity activity = new Activity("Test activity 2", project);
 		Assert.assertTrue(true);
 	}
 	
