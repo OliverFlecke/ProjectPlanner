63337137d9b5e8da37b4ba495a7cd68ecb51d0fe
Oliver Fleckenstein <Oliverfl@live.dk>
2015-05-10 22:14:11 +0200

    Update

--- tests/projectPlanner/ProjectPlannerTest.java
+++ tests/projectPlanner/ProjectPlannerTest.java
@@ -6,7 +6,9 @@ import org.junit.*;
 import org.junit.experimental.categories.Category;
 
 import projectPlanner.testCategories.*;
+import projectPlanner.users.*;
 import projectPlanner.users.UserLoginException;
+
 /**
  * @author Oliver Fleckenstein
  *
@@ -48,6 +50,27 @@ public class ProjectPlannerTest {
 			Assert.assertFalse(projectPlanner.login("Username not found", "Not relevant"));
 		} catch (UserLoginException ex) {
 			Assert.assertEquals("Username is not registed in the system.", ex.getMessage());
+		}
+	}
+	
+	@Test
+	@Category(UserStoryTest.class)
+	public void createActivityAsProjectLeader() throws SQLException {
+		// Setup
+		User user = new Employee("Oliver", "1234", "Oliver", "Fleckenstein", 2);
+		Project project = null;
+		try {
+			project = Project.getProject(1);
+		} catch (SQLException ex) {
+			Assert.fail("Exception should not be thrown here. This is an issue in the project class");
+		}
+		
+		// Create new activity 
+		try {
+			// The activity will insure it gets added to the passed project
+			Activity activity = new Activity("New usercase project", project);		
+		} catch (SQLException ex) { 
+			Assert.fail("Exception should not be thrown here.");
 		}
 	}
 }
